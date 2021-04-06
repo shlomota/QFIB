f065e1043ad805d9757a274f818b37834fc8f29c
shlomota <shlomotannor@mail.tau.ac.il>
2021-04-07 02:57:47 +0300

    train 40 epochs

--- alephbert_finetune.py
+++ alephbert_finetune.py
@@ -1,8 +1,16 @@
 from transformers import pipeline
+import logging
+
+
+# fill_mask = pipeline(
+#     "fill-mask",
+#     model="onlplab/alephbert-base",
+#     tokenizer="onlplab/alephbert-base"
+# )
 fill_mask = pipeline(
     "fill-mask",
-    model="onlplab/alephbert-base",
-    tokenizer="onlplab/alephbert-base"
+    model="./alephbert",
+    tokenizer="./alephbert"
 )
 
 from transformers import LineByLineTextDataset
@@ -11,6 +19,12 @@ model = fill_mask.model
 dataset = LineByLineTextDataset(
     tokenizer=tokenizer,
     file_path="./data/full_training_set.txt",
+    block_size=128,
+)
+
+eval_dataset = LineByLineTextDataset(
+    tokenizer=tokenizer,
+    file_path="./data/test_data.txt",
     block_size=128,
 )
 
@@ -26,7 +40,7 @@ from transformers import Trainer, TrainingArguments
 training_args = TrainingArguments(
     output_dir="./alephbert",
     overwrite_output_dir=True,
-    num_train_epochs=1,
+    num_train_epochs=40,
     per_gpu_train_batch_size=16,
     save_steps=10_000,
     save_total_limit=2,
@@ -41,5 +55,15 @@ trainer = Trainer(
 )
 
 trainer.train()
+
+logging.info("Started evaluation on train!")
+metrics = trainer.evaluate()
+logging.info(metrics)
+print(metrics)
+
+logging.info("Started evaluation on test!")
+metrics = trainer.evaluate(eval_dataset=eval_dataset)
+logging.info(metrics)
+print(metrics)
 
 trainer.save_model("./alephbert")
